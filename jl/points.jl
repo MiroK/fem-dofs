2859aa1befc4d9108f16de2d6c2c3038a94b398e
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2015-07-14 18:31:40 +0200

    first julia steps. Pythons batteries included is hard to beat

---
+++ jl/points.jl
@@ -0,0 +1,28 @@
+module Points
+
+"""Degree + 1 points evenly distributed on (-1, 1). Define polynomial of degree."""
+function equidistant_points(degree::Int64)
+  if degree == 0
+    return [0.]
+  elseif degree == 1
+    return [-1., 1.]
+  else
+    return linspace(-1, 1, degree + 1)
+  end
+end
+
+
+"""Degree + 1 points Chebyshev points distributed on (-1, 1). Define polynomial of degree."""
+function chebyshev_points(degree::Int64)
+  if degree == 0
+    return [0.]
+  elseif degree == 1
+    return [-1., 1.]
+  else
+    return union([-1], [-cos(pi*(2*k-1)/(2*(degree + 1))) for k in 2:degree], [1])
+  end
+end
+
+# Add Gauss-Legendre nodes
+
+end
